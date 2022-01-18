272ed51cb426f604ade5ad454160573d73c40d64
hcm083214 <13723742351@163.com>
2022-01-18 20:26:33 +0800

    设置为demo环境便于建立演示项目

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const webpack = require("webpack");
-// const NODE_ENV = 'demo'; //设置未演示环境，数据全部来源于本地，不需要部署后端接口
-const NODE_ENV = 'development';
+const NODE_ENV = 'demo'; //设置未演示环境，数据全部来源于本地，不需要部署后端接口
+// const NODE_ENV = 'development';
 module.exports = {
     entry: './src/index.js',
     output: {
